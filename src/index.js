ed27f976eb58d048c438d9f0b2255135a21c62c2
Myznikov Vitaly <myzvitalik@yandex.ru>
2023-03-03 11:04:49 +0300

    Creating the base maket

--- src/index.js
+++ src/index.js
@@ -2,16 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/App';
 import './style/style.scss';
-import MarvelService from './services/MarvelService'
-
-let marvelService = new MarvelService();
-marvelService.getAllCharacters().then( res =>{
-  console.log(res);
-})
-marvelService.getCharacter(1011196).then(res =>{console.log(res.data)} )
-
-
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
