f3c4065a51fecd43fa17f4ed2e614376100dd53e
Myznikov Vitaly <myzvitalik@yandex.ru>
2023-03-30 17:01:46 +0300

    Переписывание классового компонента на функциональный и использование HttpHook

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,89 +1,55 @@
 import './charInfo.scss';
-import {Component} from "react";
-import MarvelService from "../../services/MarvelService";
 import Spinner from "../spinner/Spinner";
 import ErrorMessage from "../errorMessage/ErrorMessage";
 import Skeleton from "../skeleton/Skeleton";
+import {useEffect, useState} from "react";
+import useMarvelService from "../../services/MarvelService";
 
-class CharInfo extends Component {
+const CharInfo = (props) => {
 
-    state = {
-        char: null,
-        error: false,
-        loading: false
-    }
+    const [char, setChar] = useState();
 
-    componentWillMount() {
-        this.updateChar()
-    }
+    useEffect(() => {
+        updateChar();
+    }, [props.id])
 
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        if (this.props.id !== prevProps.id) {
-            this.updateChar();
-        }
-    }
 
-    marvelService = new MarvelService();
+    const {loading, error, getCharacter} = useMarvelService();
 
-    updateChar = () => {
-
-        const {id} = this.props;
+    const updateChar = () => {
+        const {id} = props;
         if (!id) {
             return;
         }
 
-        this.onCharLoading();
+        getCharacter(id)
+            .then(onLoaded)
+    }
 
-        this.marvelService
-            .getCharacter(id)
-            .then(this.onLoaded)
-            .catch(this.onError)
+    const onLoaded = (char) => {
+        setChar(char);
     }
 
 
-    
-    onCharLoading = () => {
-        this.setState({
-            loading: true
-        })
-    }
-    onLoaded = (char) => {
-        this.setState({
-            char,
-            loading: false
-        })
-    }
-
-    onError = () => {
-        this.setState({
-            error: true,
-            loading: false
-        })
-    }
-
-    render() {
-        const {loading, error, char} = this.state;
-
-        const skeleton =  char || loading || error ? null : <Skeleton/>;
-        const spinner = loading ? <Spinner/> : null;
-        const errorMessage = error ? <ErrorMessage/> : null;
-        const content = !(spinner || errorMessage || !char) ? <View char={char}/> : null;
-        return (
-            <div className="char__info">
-                {skeleton}
-                {errorMessage}
-                {spinner}
-                {content}
-            </div>
-        )
-    }
+    const skeleton = char || loading || error ? null : <Skeleton/>;
+    const spinner = loading ? <Spinner/> : null;
+    const errorMessage = error ? <ErrorMessage/> : null;
+    const content = !(spinner || errorMessage || !char) ? <View char={char}/> : null;
+    return (
+        <div className="char__info">
+            {skeleton}
+            {errorMessage}
+            {spinner}
+            {content}
+        </div>
+    )
 }
 
 const View = ({char}) => {
-    const {name, description, thumbnail, homepage, wiki, comics} = char
+    const {name, description, thumbnail, homepage, wiki, comics, id} = char
     return (
         <>
-            <div className="char__basics">
+            <div key={id}     className="char__basics">
                 <img src={thumbnail} alt="abyss"/>
                 <div>
                     <div className="char__info-name">{name}</div>
@@ -108,7 +74,7 @@ const View = ({char}) => {
                     comics.map((item, i) => {
                         if (i > 9) return;
                         return (
-                            <li className="char__comics-item">
+                            <li key={i} className="char__comics-item">
                                 {item.name}
                             </li>
                         )
